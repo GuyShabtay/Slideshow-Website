dcc4d67188ac14f50d5f884bfcff8122423095ba
Guy Shabtay <guysh16@ac.sce.ac.il>
2023-08-26 09:08:55 +0300

    page 2 change

--- script.js
+++ script.js
@@ -10,25 +10,34 @@ moveButton.addEventListener('click', () => {
   clicksCounter++;
   switch (clicksCounter) {
     case 1:
-      moveButton.classList.add('click1');    
+      moveButton.classList.add('click1');
+      moveButton.textContent = "Oops!";
+    
       break;
     case 2:
       moveButton.classList.remove('click1');
-      moveButton.classList.add('click2');    
+      moveButton.classList.add('click2');  
+      moveButton.textContent = "Miss me..";
+  
       break;
     case 3:
       moveButton.classList.remove('click2');
-      moveButton.classList.add('click3');    
+      moveButton.classList.add('click3'); 
+      moveButton.textContent = "I'm over here";
+   
       break;
     case 4:
       moveButton.classList.remove('click3');
-      moveButton.classList.add('click4');    
+      moveButton.classList.add('click4'); 
+      moveButton.textContent = "Ok, no more fooling around!";
+   
       break;
     case 5:
       moveButton.classList.remove('click4');
       moveButton.classList.add('hidden');
       next2El.classList.add('bigNext2');
-      next2El.classList.remove('hidden');    
+      next2El.classList.remove('hidden');
+    
   
       clicksCounter=0;  
       break;
